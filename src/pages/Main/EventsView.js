66eda705760b6e62d132c66b46cb51ef7a91b77f
Arun Karri <kashyap1049@gmail.com>
2018-07-04 08:32:59 +0000

    cards for home page

--- src/pages/Main/EventsView.js
+++ src/pages/Main/EventsView.js
@@ -50,54 +50,72 @@ class Events extends Component {
       //    <button onClick ={this.filterList}>Filter</button>
 
       //   </div>style={{backgroundImage: "url(" + Background + ")"}}
-      <div className="col-md-8">
-      <div className="card">
-        {/* <div style={{backgroundImage: "url(" + Background + ")"}}> */}
-        <div className="header">
-          <h4 className="title">Hackathons List</h4>
-          <p className="category">Here is a subtitle for this table</p>
-        </div>
-        <div className="content table-responsive table-full-width">
-          <table className="table table-hover table-striped">
-            <thead>
-              <tr>
-                <th>Name</th>
-                {/* <th>Maximum Participants</th> */}
-                <th>Event Start Date</th>
-                {/* <th>Event End Date</th>
-                <th>Event Description</th>
-                <th>Organization</th> */}
-                <th>Technology Stack</th>
-                {/* <th>Criteria</th> */}
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.events.map(function (event, index) {
+      // <div className="col-md-8">
+      // <div className="card">
+      //   {/* <div style={{backgroundImage: "url(" + Background + ")"}}> */}
+      //   <div className="header">
+      //     <h4 className="title">Hackathons List</h4>
+      //     <p className="category">Here is a subtitle for this table</p>
+      //   </div>
+      //   <div className="content table-responsive table-full-width">
+      //     <table className="table table-hover table-striped">
+      //       <thead>
+      //         <tr>
+      //           <th>Name</th>
+      //           {/* <th>Maximum Participants</th> */}
+      //           <th>Event Start Date</th>
+      //           {/* <th>Event End Date</th>
+      //           <th>Event Description</th>
+      //           <th>Organization</th> */}
+      //           <th>Technology Stack</th>
+      //           {/* <th>Criteria</th> */}
+      //           <th>Action</th>
+      //         </tr>
+      //       </thead>
+      //       <tbody>
+      //         {this.state.events.map(function (event, index) {
 
-                return <tr key={index}>
-                  <td><a><Link to={`/events/${event.NAME}`}>{event.NAME}</Link></a></td>
-                  {/* <td>{event.participants}</td> */}
-                  <td>{event.start_date}</td>
-                  {/* <td>{event.end_date}</td>
-                  <td>{event.description}</td>
-                  <td>{event.org}</td> */}
-                  <td>{event.tech}</td>
-                  {/* <td>
-                    <ul>
-                      {event.criteria.map(function (crt, index) {
-                        return <li key={index}>{crt}</li>
-                      })}
-                    </ul></td> */}
-                    <td><button type="button" className="btn btn-xs btn-info btn-fill">Register</button></td>
-                </tr>
-              })}
+      //           return <tr key={index}>
+      //             <td><a><Link to={`/events/${event.NAME}`}>{event.NAME}</Link></a></td>
+      //             {/* <td>{event.participants}</td> */}
+      //             <td>{event.start_date}</td>
+      //             {/* <td>{event.end_date}</td>
+      //             <td>{event.description}</td>
+      //             <td>{event.org}</td> */}
+      //             <td>{event.tech}</td>
+      //             {/* <td>
+      //               <ul>
+      //                 {event.criteria.map(function (crt, index) {
+      //                   return <li key={index}>{crt}</li>
+      //                 })}
+      //               </ul></td> */}
+      //               <td><button type="button" className="btn btn-xs btn-info btn-fill">Register</button></td>
+      //           </tr>
+      //         })}
 
-            </tbody>
-          </table>
+      //       </tbody>
+      //     </table>
+      //   </div>
+      // </div>
+      <div>
+      {this.state.events.map(function (event, index) {
+       return  <div className="col-md-4">
+          <div className="card">
+            {/* <div style={{backgroundImage: "url(" + Background + ")"}}> */}
+            <div className="header">
+              <h4 className="title"><a><Link to={`/events/${event.NAME}`}>{event.NAME}</Link></a></h4>
+              <p className="category"><b>Starts on:</b> {event.start_date}</p>
+              <p className="category"><b>Technology Stack expected:</b> {event.tech}</p>
+            
+            <button type="button" className="btn btn-info btn-fill">Register</button>
+            </div>
+          </div>
         </div>
+
+        })}
       </div>
-     </div>
+    //  </div>
+      
     );
   }
 
