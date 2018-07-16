15101a8a5eeec68609ae6508d217413cabe58411
ashapadmashetti <asha.padmashetti@gmail.com>
2018-07-16 15:40:47 +0530

    Add files via upload

--- src/pages/Main/viewSolution.js
+++ src/pages/Main/viewSolution.js
@@ -1,63 +1,14 @@
 import React, { Component } from 'react';
-import styles from '../Main/events.css';
-import { Link } from 'react-router-dom';
+import Modal from 'react-modal';
 import { Field, reduxForm } from 'redux-form';
 import renderField from 'components/FormInputs/renderField';
 
-// import Background from '../../assets/images/events.jpg';
-
-class ViewSolutionComponent extends Component {
-  constructor() {
-    super();
-    this.state = { teams: [],show:''};
-    this.filterList = this.filterList.bind(this);
-  }
-  
-  componentDidMount() {
-    // var List = [{name:"Hackathkghghjgon1",org :"Wireless"},{name:"Hackathon2",org :"Wireless4"},{name:"Hackathon3",org :"Wireline"}]
-    
-    this.callApi()
-    .then(res => 
-      {
-        
-      console.log(res); 
-      this.setState({ teams: res })
-      })      
-    .catch(err => console.log(err));
-
-};
-
-callApi = async () => {
-    // fetch('http://10.74.17.159:4500/cyberarknode/hacks')
-    const response = await fetch('http://10.74.17.159:4500/cyberarknode/teams');
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
-
-handleSubmit(e) {
-       
-       
-    console.log('submitted');
-   
-
-    e.preventDefault();
-}
-
-showPopup(){
-    this.setState({show:true})
-}
-
-
-
-  
-
-render() {
-   
+class List extends React.Component {
+  render() {
     return (
-        <div className="">
+      <div>
+          <div className="">
+         
           <div className="card">
             {/* <div style={{backgroundImage: "url(" + Background + ")"}}> */}
             <div className="header">
@@ -74,39 +25,200 @@ render() {
                   </tr>
                 </thead>
                 <tbody>
-  
-  
-                {this.state.teams.map(function (event, index) {
+        {this.props.items.map((x, i) => (
+            <tr key={i}>
+                    <td>{x.team_name}</td>
+                    <td><a href={x.solution} target="_blank">{x.solution}</a></td>
+                    <td><a>{x.code_zip}</a></td>
+                    <td><a>{x.video}</a></td>
+                    <td><button type="button"  className="btn btn-xs btn-info btn-fill" onClick={this.props.onItemClick(i)}>Give Score
+                      
+     </button> </td>
+     </tr>
+     
 
-           return <tr key={index}>
-                    <td>{event.team_name}</td>
-                    <td><a>{event.solution}</a></td>
-                    <td><a>{event.code_zip}</a></td>
-                    <td><a>{event.video}</a></td>
-                    <td><button type="button"  className="btn btn-xs btn-info btn-fill">Give Score</button> </td>
-                    
-                  </tr>
-                })}
-  
-  
-                </tbody>
+        ))}
+           </tbody>
               </table>
             </div>
           </div>
         </div>
-      );
-  }
-
-  filterList() {
-    let arra = this.state.events.filter(item => item.org === 'Wireless');
-
-    this.setState({ events: arra });
+            
+      </div>
+    );
   }
 }
 
+Modal.setAppElement('body');
+class MultipleModals extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listItemsIsOpen: false,
+      currentItem: -1,
+      loading: false,
+      selectedItem:{"hack_info":""},
+      items: [],
+      list:{"score":{}},
+      
+      
+    };
+    this.Submitscore = this.Submitscore.bind(this);
+  }
+  
+Submitscore(e) {
+   //
+   //5b3b62d2ca4aa9c842eec019
+       
+    console.log('submitted');
+   this.setState({list : {"score":{"Solution":90,"Presentation":80}}},()=>{
+       console.log(this.state.list);
+    this.cupdate().then(res => console.log('success response is',res))
+    .catch(err => console.log(err));  
+   });
+   this.setState({ currentItem: -1 });
+   // this.setState({selectedItem:this.state.items[index]});
+    this.setState({selectedItem:this.state.items[-1]}
+          , () => {
+            console.log(this.state.selectedItem);
+          })
+  
+
+    e.preventDefault();
+}
+
+cupdate = async () => {
+    
+        
+    const response = await fetch('http://localhost:4500/cyberarknode/teams/5b3b62d2ca4aa9c842eec019',{method:'PUT',headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(this.state.list)
+});
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
 
 
-export default (reduxForm({
-    form: 'hackForm',
-})(ViewSolutionComponent));
+  componentDidMount() {
+    // var List = [{name:"Hackathkghghjgon1",org :"Wireless"},{name:"Hackathon2",org :"Wireless4"},{name:"Hackathon3",org :"Wireline"}]
+    
+    this.callApi()
+    .then(res => 
+      {
+        
+      console.log(res); 
+      this.setState({ items: res })
+      })      
+    .catch(err => console.log(err));
 
+};
+
+callApi = async () => {
+    // fetch('http://localhost:4500/cyberarknode/hacks')
+    const response = await fetch('http://localhost:4500/cyberarknode/teamsMaster');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  toggleModal = event => {
+    event.preventDefault();
+    if (this.state.listItemsIsOpen) {
+      this.handleModalCloseRequest();
+      return;
+    }
+    this.setState({
+      items: [],
+      listItemsIsOpen: true,
+      loading: true
+    });
+  }
+
+  handleModalCloseRequest = () => {
+    // opportunity to validate something and keep the modal open even if it
+    // requested to be closed
+    this.setState({
+      listItemsIsOpen: false,
+      loading: false
+    });
+  }
+/*
+  handleOnAfterOpenModal = () => {
+    // when ready, we can access the available refs.
+    (new Promise((resolve, reject) => {
+      setTimeout(() => resolve(true), 500);
+    })).then(res => {
+      this.setState({
+        items: [1, 2, 3, 4, 5].map(x => `Item ${x}`),
+        loading: false
+      });
+    });
+    
+  }*/
+
+  onItemClick = index => event => {
+    this.setState({ currentItem: index });
+   // this.setState({selectedItem:this.state.items[index]});
+    this.setState({selectedItem:this.state.items[index]}
+          , () => {
+            console.log(this.state.selectedItem);
+          })
+  
+  }
+
+  cleanCurrentItem = () => {
+    this.setState({ currentItem: -1 });
+  }
+
+  render() {
+    const { listItemsIsOpen } = this.state;
+    return (
+      <div>
+                
+  <List onItemClick={this.onItemClick} items={this.state.items} />
+      {this.state.selectedItem ? <Modal
+          id="test2"
+          closeTimeoutMS={150}
+          contentLabel="modalB"
+          isOpen={this.state.currentItem > -1}
+          onRequestClose={this.cleanCurrentItem}
+          aria={{
+            labelledby: "item_title",
+            describedby: "item_info"
+          }}>
+          <h1 id="item_title">Scoring for - {this.state.selectedItem.team_name}</h1>
+          <div id="item_info">
+              {  this.state.selectedItem.hack_info ? this.state.selectedItem.hack_info[0].criteria.map((x, i) => (
+            <div key={i}>
+                    <div className="col-sm-4">{x}</div>
+                     <div className="form-group">
+                                 
+                                    <input
+                                        name="score"
+                                        type="number" ref="score"
+                                       />
+                                </div>         
+                      
+    
+    </div>
+   
+     
+
+        ) ) : null}
+         <button type="submit" className="btn btn-fill btn-info" onClick={this.Submitscore}>Submit</button>
+         
+          </div>
+        </Modal> :null }
+      </div>
+    );
+  }
+}
+
+export default MultipleModals
